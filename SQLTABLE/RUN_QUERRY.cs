d44c31898d9a1ab202eb94a43df3579126d02b95
Agentnick <nmagkoynis5@hotmail.com>
2016-05-04 17:21:46 +0300

    FIrst

---
+++ SQLTABLE/RUN_QUERRY.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace SQLTABLE
+{
+   public class RUN_QUERRY
+    {
+       public RUN_QUERRY()
+       {
+           querry = "SELECT STATE FROM WHMUSERS";
+            connectionstring = "Server=MGK_LAPTOP;Database=ACTDB;User Id=sa;Password=actae1234!@#$;";
+           SqlConnection sqlcon = new SqlConnection();
+           sqlcon.ConnectionString = connectionstring;
+           SqlCommand sqlcmd = new SqlCommand(querry,sqlcon);
+           try
+           {
+               sqlcon.Open();
+               SqlDataReader sqldtrd = sqlcmd.ExecuteReader();
+             //  sqldtrd = 
+               if (sqldtrd.HasRows)
+               {
+                   sqldtrd.Read();
+                  state = sqldtrd.GetBoolean(0);
+                  
+                   sqlcmd.ExecuteNonQuery();
+                   
+               }
+
+           }
+
+           catch (Exception e)
+           {
+
+
+           }
+           
+
+       }
+
+       public string connectionstring { get; set; }
+       public string querry { get; set; }
+       public bool state { get; set; }
+
+
+
+    }
+}
